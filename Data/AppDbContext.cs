c542c0312c39ff3586d5c4be52dfaa1dd1b98669
khanimgurbanli <zeri.q.222@gmail.com>
2022-09-23 08:42:07 +0400

    second commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStar_.Models;
+
+namespace WebStar_.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public  DbSet<Product> Products { get; set; }
+        public  DbSet<Images> Images { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebStar_.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>
+           (option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
